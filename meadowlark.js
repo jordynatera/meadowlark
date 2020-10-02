bcb3336429a36a65577e4e599f4dab7ad7343741
jordynatera <jordynatera@users.noreply.github.com>
2020-10-02 10:20:05 -0400

    Utilizando Express y handlebars

--- meadowlark.js
+++ meadowlark.js
@@ -1,34 +1,34 @@
+//import express from 'express';
 var express = require('express');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-var handlebars = require('express3-handlebars')
-    .create({ defaultLayour:'main' });
+var handlebars = require('express3-handlebars')//importando handlebars
+    .create({ defaultLayout:'main' });//layout para las vistas
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-
+//routes
 app.get('/', function(req, res){
-    res.type('text/plain');
-    res.send('Meadowlark Travel');
+    //res.type('text/plain'); tipo de contenido
+    //res.send('Meadowlark Travel'); enviar msj
+    res.render('home'); 
 });
 
 app.get('/about', function(req, res){
-    res.type('text/plain');
-    res.send('About Meadowlark Travel')
-})
-
-app.use(function(req, res) {
-    res.type('text/plain');
+    res.render('about');
+});
+//middleware for 404 Not Found/ Catch-all handler
+app.use(function(req, res, next) {
     res.status(404);
-    res.send('404 / Not Found');
+    res.render('404');
 });
-
-app.use(function(req, res){
-    res.type('text/plain');
+//middleware for 500 - Server Error
+app.use(function(err, req, res, next){
+    console.error(err.stack);
     res.status(500);
-    res.send('500 / Server Error');
-});
-app.listen(app.get('port'), function(){
+    res.render('500');
+}); 
+app.listen(app.get('port'), function(){//set listen port
     console.log( 'Express started on http://localhost:'+app.get('port')+'; press Ctrl C to terminate.');
 });
